7507387b42847105cc62a81aa778ff5b4aebf859
Pushpendra Singh <pushpendra7060@gmail.com>
2020-07-16 13:58:10 +0530

    controlle form handler is added

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,16 +5,49 @@ class Login extends React.Component {
     {
         super(props);
 
-        this.emailInputRef = React.createRef();
-        this.passwordInputRef = React.createRef();
+      /*  
+      //this was for uncontrolled component handled
+      this.emailInputRef = React.createRef();
+        this.passwordInputRef = React.createRef();*/
+
+        this.state = {
+
+            email:'',
+            password:''
+        }
+    }
+
+    handleEmailChange = (e) => {
+
+       // console.log('Email',e.target.value)
+
+        this.setState({
+            email:e.target.value
+        })
+
+
+    }
+
+    handlePasswordChange = (e) => {
+
+       // console.log('Password',e.target.value);
+
+        this.setState({
+            password:e.target.value
+        })
+
+
+
     }
 
     handleFormSubmit = (e) => {
         e.preventDefault();
 
-        console.log('this.emailInputRef', this.emailInputRef);
+        /* console.log('this.emailInputRef', this.emailInputRef);
 
-        console.log('this.passwordInputRef', this.passwordInputRef)
+         console.log('this.passwordInputRef', this.passwordInputRef)*/
+
+         console.log('Your state is', this.state)
     }
     render() {
         return (
@@ -22,13 +55,25 @@ class Login extends React.Component {
                 <span className="login-signup-header">Log In</span>
                 <div className="field">
 
-                    <input type="email" placeholder="Email" ref= {this.emailInputRef} required />
+                    <input type="email" placeholder="Email" 
+                    //ref= {this.emailInputRef}  //this was for uncontrolled component form handle
+
+                    onChange = {this.handleEmailChange}
+
+                    value = {this.state.email}
+                     required />
 
                 </div>
 
                 <div className="field">
                     
-                <input type="password" placeholder="Password" ref={this.passwordInputRef} required />
+                <input type="password" placeholder="Password" 
+                //ref={this.passwordInputRef} //this was for uncontrolled component form handle
+
+                onChange = {this.handlePasswordChange}
+
+                value = {this.state.password}
+                 required />
 
                 </div>
 
